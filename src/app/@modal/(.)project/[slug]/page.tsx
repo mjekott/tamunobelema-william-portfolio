39da26bbf0fa9b3f9ca044871c9bbb8a1690929d
mjekott <ekottmfon@yahoo.com>
2023-08-05 17:42:55 +0100

    performace(Modal): cleanup code

--- src/app/@modal/(.)project/[slug]/page.tsx
+++ src/app/@modal/(.)project/[slug]/page.tsx
@@ -1,31 +1,7 @@
 import Modal from "@/components/Modal";
 import ProjectImageSlide from "@/features/projects/ProjectImageSlide";
 import BackButton from "@/layout/BackButton";
-import { groq } from "next-sanity";
-import { clientFetch } from "../../../../../sanity/config/sanity.client";
 import { getProject } from "../../../../../sanity/sanity-utils";
-
-// Generate Static Params Function
-export const generateStaticParams = async () => {
-  try {
-    const projects = await clientFetch(groq`*[_type=='project'] [0...20] {
-      'slug': slug.current,
-    }`);
-
-    const params = projects.map((project: any) => {
-      return {
-        slug: project.slug,
-      };
-    });
-
-    return params;
-  } catch (error) {
-    console.log(error);
-    throw new Error("Error fetching projects");
-  }
-};
-
-export const revalidate = 600;
 
 const page = async ({ params }: { params: { slug: string } }) => {
   const project = await getProject(params.slug);
